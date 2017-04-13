36f8782f79903539e1c39f7ebe932a9e14cfe456
fducau <fnd212@nyu.edu>
2017-04-13 18:11:28 -0400

    Adding python3

--- runall.sh
+++ runall.sh
@@ -7,5 +7,9 @@ python ./pytorch/vae/main.py
 echo 'Running Tensorflow Examples'
 python tensorflow/vae.py
 python tensorflow/CNN.py
+python3 tensorflow/vae.py
+python3 tensorflow/CNN.py
+
 echo 'Running Theano example'
 THEANO_FLAGS=device=gpu,floatX=float32 python theano/convolutional_mlp.py
+THEANO_FLAGS=device=gpu,floatX=float32 python3 theano/convolutional_mlp.py
